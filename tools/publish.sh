783b46f1b51e32cbf06e356ed18e2638cc7a5c83
Joshua Poehls <joshua@zduck.com>
2014-10-24 16:03:17 -0500

    WIP publish script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 # Make sure we are on master
-git checkout -f master
+# git checkout -f master
 
-TMP=`mktemp -d`
-
-hugo --destination=$(TMP)
+hugo
 
 # Switch to the gh-pages branch
 git checkout -f gh-pages
+
+# Delete all existing files
+rm -rf *
+
+# Copy the new files from the temp folder
+cp -Rf public/ .
+rm -rf public
+
+# Commit the changes
+git add -A
+git commit -m "Updated site"
+
+# Move back to the master branch
+git checkout -f master
+git clean -fd
